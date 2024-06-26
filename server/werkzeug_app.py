b11d7c6afa3263cfc6645904f2d9c594bb680594
losinyen <nachuchphilip@gmail.com>
2024-06-26 20:29:11 +0300

    1 passed

--- server/werkzeug_app.py
+++ server/werkzeug_app.py
@@ -1,1 +1,12 @@
-#!/usr/bin/env python3
+# server/werkzeug_app.py
+
+from werkzeug.wrappers import Request, Response # type: ignore
+from werkzeug.serving import run_simple # type: ignore
+
+@Request.application
+def application(request):
+    print(f'This web server is running at {request.remote_addr}')
+    return Response('A WSGI generated this response!')
+
+if __name__ == '__main__':
+    run_simple('localhost', 5555, application)
